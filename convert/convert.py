4a50d9f9c0f8c7eba3f8b01a3f5ca731c14ae477
Ruthvik Vijayakumar <coderuth@outlook.com>
2017-04-26 21:30:57 +0530

    done with file conversion

---
+++ convert/convert.py
@@ -0,0 +1,31 @@
+import os
+import glob
+import win32com.client
+from subprocess import check_output
+
+
+class convertPPT(object):
+    def __init__(self, files):
+        self.files = glob.glob(os.path.abspath("../slides/%s" % files))
+        self.powerpoint = win32com.client.Dispatch("Powerpoint.Application")
+
+    def convertPDF(self, files, formatType=32):
+        for filename in files:
+            newName = os.path.splitext(filename)[0] + ".pdf"
+            deck = self.powerpoint.Presentations.Open(filename, WithWindow=False)
+            deck.SaveAs(newName, formatType)
+            deck.Close()
+        self.powerpoint.Quit()
+
+    def convertHTML(self):
+        check_output("pdf2htmlEX --embed cfijo --fit-width 1024 --dest-dir ../slides/html "
+                     + os.path.splitext(self.files[0])[0] + ".pdf")
+
+    def convertFile(self):
+        try:
+            self.convertPDF(self.files)
+            self.convertHTML()
+        except Exception:
+            return False
+        return True
+
